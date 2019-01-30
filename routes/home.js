39dee57945bda4793fe98bba34e336bc583913f0
pasha <returnto001@gmail.com>
2019-01-30 18:17:40 +0200

    Add info vievs

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,34 @@
 const Router = require('koa-router');
 
-const router = new Router({ prefix: '/' });
+const router = new Router();
 const handler = {
   async homePage(ctx) {
     await ctx.render('index', {});
+  },
+  async eventsPage(ctx) {
+    await ctx.render('events', {});
+  },
+  async sermonsPage(ctx) {
+    await ctx.render('sermons', {});
+  },
+  async blogPage(ctx) {
+    await ctx.render('blog', {});
+  },
+  async contactPage(ctx) {
+    await ctx.render('contact', {});
+  },
+  async aboutPage(ctx) {
+    await ctx.render('about', {});
+  },
+  async sermonsDetailsPage(ctx) {
+    await ctx.render('sermons-details', {});
   }
 };
 router.get('/', handler.homePage);
+router.get('/events', handler.eventsPage);
+router.get('/sermons', handler.sermonsPage);
+router.get('/blog', handler.blogPage);
+router.get('/contact', handler.contactPage);
+router.get('/about', handler.aboutPage);
+router.get('/sermons-details', handler.sermonsDetailsPage);
 module.exports = router.routes();
